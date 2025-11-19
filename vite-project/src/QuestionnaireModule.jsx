ec3d70ad1404ef061e4c94c7dda148b0d2bc22db
Daniel-School <daniel.kong@student.tdsb.on.ca>
2025-11-19 17:17:34 -0500

    login check on questionnaire module

--- vite-project/src/QuestionnaireModule.jsx
+++ vite-project/src/QuestionnaireModule.jsx
@@ -9,6 +9,15 @@ function QuestionnaireModule(){
     const [outcomes, setOutcomes] = React.useState([]);
     const [answers, setAnswers] = React.useState([]);
     const [points, setPoints] = React.useState(0);
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [questionnaireEnd, setQuestionnaireEnd] = React.useState(false);
+
+    function checkLoggedIn(){
+        if (loggedIn == false){
+            const search = document.cookie.match(/account_id=\d+/)
+            if (search) setLoggedIn(true)
+        }
+    }
 
     //handler for answer buttons
     function answerButton(answerPoints){
@@ -17,7 +26,7 @@ function QuestionnaireModule(){
     }
 
     function printOutcome(){
-        if (questionNumber >= questions.length){
+        if (loggedIn && questionNumber >= questions.length){
             console.log("RESULTS")
             for(const outcome of outcomes){
                 if (points >= outcome.min_score && points <= outcome.max_score){
@@ -31,28 +40,55 @@ function QuestionnaireModule(){
         }
     }
 
+    function printQuestion(){
+        if (loggedIn && questionNumber < questions.length){
+            const currentQuestion = questions.filter(question => question.question_id == questions[questionNumber]. question_id)
+            const out = currentQuestion.map(question=>(
+                <h1 key={question.question_id}> {question.question_text} </h1>
+            ))
+            return out
+        }
+    }
+
+    function printAnswer(){
+        if (loggedIn && questionNumber < questions.length){
+            const currentAnswers = answers.filter(answer => answer.question_id == questions[questionNumber].question_id)
+            const out = currentAnswers.map(answer=>(
+                <input type="button" key={answer.answer_id} value={answer.answer_text} onClick={() => {answerButton(answer.answer_points)}}/> 
+            ))
+            return out
+        }
+    }
+
     //get all questions and answers from database
     React.useEffect(() =>{
         const getQuestions = async()=>{
-            //const res = await axios.get("https://cps731.onrender.com/question");
-            const res = await axios.get("http://localhost:8080/question");
-            console.log(res.data);
-            setQuestions(res.data[0])
+            if (!questions.length){
+                //const res = await axios.get("https://cps731.onrender.com/question");
+                const res = await axios.get("http://localhost:8080/question");
+                console.log(res.data);
+                setQuestions(res.data[0])
+            }
         }
 
         const getAnswers = async()=>{
-            //const res = await axios.get("https://cps731.onrender.com/answer")
-            const res = await axios.get("http://localhost:8080/answer")
-            console.log(res.data)
-            setAnswers(res.data[0])
+            if (!answers.length){
+                //const res = await axios.get("https://cps731.onrender.com/answer")
+                const res = await axios.get("http://localhost:8080/answer")
+                console.log(res.data)
+                setAnswers(res.data[0])
+            }
         }
 
         const getOutcomes = async()=>{
-            const res = await axios.get("http://localhost:8080/outcome")
-            console.log(res.data)
-            setOutcomes(res.data[0])
+            if (!outcomes.length){
+                const res = await axios.get("http://localhost:8080/outcome")
+                console.log(res.data)
+                setOutcomes(res.data[0])
+            }
         }
 
+        checkLoggedIn()
         getQuestions()
         getAnswers()
         getOutcomes()
@@ -60,20 +96,15 @@ function QuestionnaireModule(){
 
 
     return (<>
-        {questionNumber < questions.length &&
-        questions.filter((question) => question.question_id == questions[questionNumber].question_id).map(question=>(
-            <h1 key={question.question_id}> {question.question_text} </h1>
-        ))}
+        {printQuestion()}
 
         <form>
-        {questionNumber < questions.length &&
-        answers.filter((answer) => answer.question_id == questions[questionNumber].question_id).map(answer=>(
-            <input type="button" key={answer.answer_id} value={answer.answer_text} onClick={() => {answerButton(answer.answer_points)}}/> 
-        ))}
+        {printAnswer()}
         </form>
 
         {printOutcome()}
-        <h2>Points: {points}</h2>
+        {loggedIn && <h2>Points: {points}</h2>}
+        {!loggedIn && <h1>Not Logged In !!!</h1>}
     </>)
 }
 
