8968f3a74fc93df40048c31d4d6aeba2b52b5b14
Sofia Pham <sofia.pham@torontomu.ca>
2025-11-26 14:58:39 -0500

    changed routes and auth for frontend

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -7,18 +7,37 @@ import QuestionnaireModule from "./QuestionnaireModule.jsx";
 import Admin from "./Admin.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import AdminAnswer from "./AdminAnswer.jsx";
+import UserProfileModule from "./UserProfileModuleDisplay.jsx";
 import UserProfileModuleDisplay from "./UserProfileModuleDisplay.jsx";
 import AdminQuestion from "./AdminQuestion.jsx";
+import LogIn from "./LogIn.jsx";
+import SignUp from "./SignUp.jsx";
+import { AuthProvider } from "./AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          {/* <Route path="/" element={<App />} />
       <Route path="/questionnairemodule" element={<QuestionnaireModule />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/admin/answer" element={<AdminAnswer />} />
       <Route path="/admin/question" element={<AdminQuestion />} />
-      <Route path="/user" element={<UserProfileModuleDisplay />} />
-    </Routes>
-  </BrowserRouter>
+      <Route path="/user" element={<UserProfileModuleDisplay />} /> */}
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/answer" element={<AdminAnswer />} />
+          <Route path="/admin/question" element={<AdminQuestion />} />
+          <Route path="/user" element={<UserProfileModule />} />
+          <Route
+            path="/questionnairemodule"
+            element={<QuestionnaireModule />}
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
 );
